e7f3343c0f64d0df80013c43d8b0a1b4e81e05ed
kimuratakuou <ruia1082halfnc@gmail.com>
2026-05-01 12:57:43 +0900

    テスト環境実装

--- app/src/main/java/com/takaobrog/apicomposetask/presentation/MainActivity.kt
+++ app/src/main/java/com/takaobrog/apicomposetask/presentation/MainActivity.kt
@@ -11,19 +11,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.takaobrog.apicomposetask.presentation.ui.theme.ApiComposeTaskTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel : MainViewModel = hiltViewModel()
             ApiComposeTaskTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    viewModel.test()
                 }
             }
         }
