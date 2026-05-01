95cb03742dab5b94365e257255f8345fd5ad5506
kimuratakuou <ruia1082halfnc@gmail.com>
2026-05-01 09:52:23 +0900

    moshi追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.google.hilt.android)
     ksp(libs.google.hilt.compiler)
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
