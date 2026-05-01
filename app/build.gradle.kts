42fd0d127f4a0e046ce1a42b6585fc18e91dadda
kimuratakuou <ruia1082halfnc@gmail.com>
2026-05-01 09:12:22 +0900

    バージョンコードアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.takaobrog.apicomposetask"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.takaobrog.apicomposetask"
-        minSdk = 35
-        targetSdk = 35
+        minSdk = 29
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
